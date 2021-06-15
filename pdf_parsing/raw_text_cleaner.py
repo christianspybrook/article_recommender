dd86c98b463c38d3bc7d94726c90fe0f19a00f0f
christianspybrook <christianspybrook@gmail.com>
2021-06-14 17:18:42 -0700

    Add stopword removal

--- pdf_parsing/raw_text_cleaner.py
+++ pdf_parsing/raw_text_cleaner.py
@@ -1,31 +1,43 @@
 import re
 import string
 import os
+from gensim.parsing.preprocessing import STOPWORDS
 
 # retrieve my home directory
 local_file = '../home_directory/home_dir.txt'
 with open(local_file, 'r') as file:
-    my_home = file.read().rstrip('\r\n')
+	my_home = file.read().rstrip('\r\n')
 
-# choose directories to clean
-data_directories = ['0704', '2105']
+# set RAID 0 array mount path
+raid_path = my_home + '/mnt/4T_nvme'
+# set repository folder path
+repo_path = my_home + '/github/article_recommender'
 
-# choose stages of cleaning to analyze
-text_folders = ['sample_100_raw_text', 
-				'sample_100_no_hexadecimal', 
-				'sample_100_with_short_lines', 
-				'sample_100_with_new_lines', 
-				'sample_100_with_stopwords', 
-				'sample_100_cleaned']
+# select directories to clean
+data_directories = {0: '/0704', 
+					1: '/2105'
+					}
 
 # capture month and year directory to clean
 d = 1
+
+# map stages of cleaning to separate folders for analysis
+text_folders = {0: '/sample_data' + data_directories[d] + '/sample_100_raw_text', 
+				1: '/sample_data' + data_directories[d] + '/sample_100_no_hexadecimal', 
+				2: '/sample_data' + data_directories[d] + '/sample_100_with_short_lines', 
+				3: '/sample_data' + data_directories[d] + '/sample_100_with_new_lines', 
+				4: '/sample_data' + data_directories[d] + '/sample_100_with_stopwords', 
+				5: '/data' + data_directories[d] + '/full_month_with_stopwords', 
+				6: '/sample_data' + data_directories[d] + '/sample_100_cleaned', 
+				7: '/data' + data_directories[d] + '/full_month_cleaned'
+				}
+
 # capture cleaning stage to perform
-f = 4
+f = 7
 
 # set text directory paths
-read_directory = my_home + '/mnt/4T_nvme/arxiv_data/raw_text_latest/' + data_directories[d]
-write_directory = my_home + '/github/article_recommender/sample_data/' + data_directories[d] + '/' + text_folders[f]
+read_directory = raid_path + '/arxiv_data/raw_text_latest' + data_directories[d]
+write_directory = repo_path + text_folders[f]
 
 # define ligature mapping to restore words
 ligatures = {'': 'ff', 
@@ -40,7 +52,7 @@ hex_string = ''
 pattern = '[' + hex_string + ']'
 
 def read_file(file):
-	"""Returns read in document"""
+	"""Returns document read in by line"""
 	read_path = read_directory + '/' + file
 	with open(read_path, 'r') as f_in:
 		# read in text file
@@ -85,16 +97,31 @@ def clean_file(file):
 		if len(new_string) > 3:
 			# remove whitespace and new line character
 			new_string = re.sub('\s+', ' ', new_string)
+			# remove stopwords
+			new_string = ' '.join([t for t in new_string.split() if t not in STOPWORDS])
 			# add filtered string to new string list
 			new_doc.append(new_string)
 	# save new cleaned file
 	write_new_file(file, new_doc)
 
+def show_progress(num_files):
+	"""Prints files left to process"""
+	if num_files % 100 == 0:
+		print(num_files, end='')
+		print('\r', end='')
+
+
+
 
 if __name__ == '__main__':
 
-	# iterate through and clean first 100 files from May 2021
-	for file in sorted(os.listdir(read_directory))[:100]:
+	# set limit to number of files to clean
+	n_files = None
+	files_to_clean = sorted(os.listdir(read_directory))[:n_files]
+	files_left_to_clean = len(files_to_clean)
+
+	# iterate through and clean first 100 files from given folder
+	for file in files_to_clean:
+		show_progress(files_left_to_clean)
 		clean_file(file)
-
-
+		files_left_to_clean += -1
